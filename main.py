6cff8dfd8ea70df503b5a831a5eccfa1efd54e45
ygorPaulo074 <paulooliveiraarauj@gmail.com>
2026-05-10 19:09:32 -0300

    include /agent-testat main.py file

--- main.py
+++ main.py
@@ -25,3 +25,8 @@ app.include_router(agent_router)
 app.include_router(chat_router)
 app.include_router(data_router)
 app.include_router(admin_router)
+
+
+@app.get("/agent-test", include_in_schema=False)
+def chat_ui():
+    return FileResponse(_STATIC / "chat.html")
